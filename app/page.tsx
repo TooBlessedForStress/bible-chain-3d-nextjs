6dc957b6d9602f1995b6bb343ac43c9703e4612b
TooBlessedForStress <tooblessedforstressshop@gmail.com>
2026-04-18 07:18:25 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,20 @@
 "use client";
 
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { BibleChain3D } from "@/components/BibleChain3D";
 import { CreateBlockPanel } from "@/components/CreateBlockPanel";
 
-const wallets = [new PhantomWalletAdapter()];
-
 export default function Home() {
   return (
-    <ConnectionProvider endpoint="https://api.devnet.solana.com">
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <div className="min-h-screen bg-black text-white flex flex-col md:flex-row">
-            {/* 3D Viewer */}
-            <div className="flex-1 relative min-h-[600px]">
-              <BibleChain3D />
-            </div>
+    <div className="min-h-screen bg-black text-white flex flex-col md:flex-row">
+      {/* 3D Viewer - Takes most of the screen */}
+      <div className="flex-1 relative min-h-[600px]">
+        <BibleChain3D />
+      </div>
 
-            {/* Sidebar Controls */}
-            <div className="w-full md:w-96 bg-zinc-950 border-t md:border-l border-white/10 p-6 overflow-auto">
-              <CreateBlockPanel />
-            </div>
-          </div>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+      {/* Sidebar Controls */}
+      <div className="w-full md:w-96 bg-zinc-950 border-t md:border-l border-white/10 p-6 overflow-auto">
+        <CreateBlockPanel />
+      </div>
+    </div>
   );
 }
