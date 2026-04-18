0243bd865e8539d4dc11f00bd4d314280a406c83
TooBlessedForStress <tooblessedforstressshop@gmail.com>
2026-04-18 15:23:07 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,13 @@ const CreateBlockPanel = dynamic(
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white flex flex-col lg:flex-row overflow-hidden">
-      {/* 3D Scene - Takes the majority of the screen */}
-      <div className="flex-1 relative min-h-[600px] lg:min-h-screen">
+      {/* 3D Scene - Takes almost the entire screen */}
+      <div className="flex-1 relative min-h-[700px] lg:min-h-screen">
         <BibleChain3D />
       </div>
 
-      {/* Sidebar - Fixed on the right on large screens */}
-      <div className="w-full lg:w-96 bg-zinc-950 border-t lg:border-l border-white/10 p-8 overflow-auto">
+      {/* Sidebar - Fixed width on large screens, full width on mobile */}
+      <div className="w-full lg:w-96 bg-zinc-950 border-t lg:border-l border-white/10 p-8 overflow-auto lg:min-h-screen">
         <CreateBlockPanel />
       </div>
     </div>
