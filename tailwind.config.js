3d9d6c94e16105f5664b3b5e052563967e7dd2c9
TooBlessedForStress <tooblessedforstressshop@gmail.com>
2026-04-18 16:19:32 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        holy: "#d4af77",     // gold
+        darkbg: "#0a0a0a",
+      },
+    },
+  },
   plugins: [],
 };
+export default config;
