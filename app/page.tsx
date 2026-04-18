4f377ae7426b5df7f6800b38e0e8e2c95e4a66f1
TooBlessedForStress <tooblessedforstressshop@gmail.com>
2026-04-18 07:27:24 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ import { CreateBlockPanel } from "@/components/CreateBlockPanel";
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white flex flex-col md:flex-row">
-      {/* 3D Viewer - Takes most of the screen */}
+      {/* 3D Viewer */}
       <div className="flex-1 relative min-h-[600px]">
         <BibleChain3D />
       </div>
 
-      {/* Sidebar Controls */}
+      {/* Sidebar */}
       <div className="w-full md:w-96 bg-zinc-950 border-t md:border-l border-white/10 p-6 overflow-auto">
         <CreateBlockPanel />
       </div>
